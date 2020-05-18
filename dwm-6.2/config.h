53b3f58dfc1a7290fc749dade4a52588901bf087
Hyper-KVM <hyperkvmx86@gmail.com>
2020-05-18 08:09:54 -0400

    make config.h more readable, fix silly mistake with sed command add pulseaudio daemon at startup

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -45,72 +45,78 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
+/* volume control */
 static const char *mutecmd[] = { "./.volcontrol/mute", NULL };
 static const char *volupcmd[] = { "./.volcontrol/volup", NULL };
 static const char *voldowncmd[] = { "./.volcontrol/voldown", NULL };
+/* brightness control */
 static const char *brupcmd[] = { "./.backlightcontrol/backlightup", NULL };
 static const char *brdowncmd[] = { "./.backlightcontrol/backlightdown", NULL };
+/* media control */
 static const char *tracknext[] = { "playerctl", "next", NULL };
 static const char *trackprev[] = { "playerctl", "previous", NULL };
 static const char *trackplaypause[] = { "playerctl", "play-pause", NULL };
+/* screenshot control */
 static const char *fullscreenshot[] = { "scrot", "-e", "\''mv $f Pictures/'\'", "-d", "2", NULL };
 static const char *windowscreenshot[] = { "scrot", "-e", "\''mv $f Pictures/'\'", "-u", "-d", "2", NULL };
 static const char *selscreenshot[] = { "./.importscript", NULL };
+/* system info */
 static const char *sysinfo[] = { "sh", ".sysxroot", NULL };
 static const char *xrandrkey[] = { "./.xrandrconf", NULL };
+/* screen lock */
+static const char *slock[] = { "slock", NULL };
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ 0, 				XF86XK_AudioMute, spawn, {.v = mutecmd } },
-	{ 0, 				XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
-	{ 0, 				XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
-	{ 0, 				XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
-	{ 0, 				XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd} },
-	{ 0, 				XF86XK_AudioNext, spawn, {.v = tracknext} },
-	{ 0, 				XF86XK_AudioPrev, spawn, {.v = trackprev} },
-	{ 0, 				XF86XK_AudioPlay, spawn, {.v = trackplaypause} },
-	{ MODKEY,			XK_p, spawn, {.v = xrandrkey} },
-	{ 0,				XK_Print, spawn, {.v = fullscreenshot} },
-	{ MODKEY,			XK_Print, spawn, {.v = windowscreenshot} },
-	{ MODKEY|ShiftMask,		XK_Print, spawn, {.v = selscreenshot} },
-	{ MODKEY,			XK_s, spawn, {.v = sysinfo} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key        			function        argument */
+	{ MODKEY,                       XK_o,      			spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_b,      			togglebar,      {0} },
+	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      			setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      			setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, 			zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    			view,           {0} },
+	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  			setlayout,      {0} },
+	{ MODKEY,                       XK_0,      			view,           {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  			focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 			focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  			tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 			tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_c,      			killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,  			togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_0,      			tag,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_Return, 			spawn,		{.v = termcmd } },
+	{ MODKEY,			XK_p, 				spawn, 		{.v = xrandrkey} },
+	{ MODKEY,			XK_s, 				spawn, 		{.v = sysinfo} },
+	{ 0, 				XF86XK_AudioMute, 		spawn, 		{.v = mutecmd } },
+	{ 0, 				XF86XK_AudioLowerVolume, 	spawn, 		{.v = voldowncmd } },
+	{ 0, 				XF86XK_AudioRaiseVolume, 	spawn, 		{.v = volupcmd } },
+	{ 0, 				XF86XK_MonBrightnessUp, 	spawn, 		{.v = brupcmd} },
+	{ 0, 				XF86XK_MonBrightnessDown, 	spawn, 		{.v = brdowncmd} },
+	{ 0, 				XF86XK_AudioNext, 		spawn, 		{.v = tracknext} },
+	{ 0, 				XF86XK_AudioPrev, 		spawn, 		{.v = trackprev} },
+	{ 0, 				XF86XK_AudioPlay, 		spawn, 		{.v = trackplaypause} },
+	{ 0,				XK_Print, 			spawn, 		{.v = fullscreenshot} },
+	{ MODKEY,			XK_Print, 			spawn, 		{.v = windowscreenshot} },
+	{ MODKEY|ShiftMask,		XK_Print, 			spawn, 		{.v = selscreenshot} },
+	TAGKEYS(                        XK_1,                      			0)
+	TAGKEYS(                        XK_2,                      			1)
+	TAGKEYS(                        XK_3,                      			2)
+	TAGKEYS(                        XK_4,                      			3)
+	TAGKEYS(                        XK_5,                      			4)
+	TAGKEYS(                        XK_6,                      			5)
+	TAGKEYS(                        XK_7,                      			6)
+	TAGKEYS(                        XK_8,                      			7)
+	TAGKEYS(                        XK_9,                      			8)
+	{ MODKEY|ShiftMask,             XK_q,      			quit,		{0} },
 };
 
 /* button definitions */
