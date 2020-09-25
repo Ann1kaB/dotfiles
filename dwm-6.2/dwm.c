a95f01b50be21886b68cbe67a14dd22895287f1a
Hyper-KVM <hyperkvmx86@gmail.com>
2020-09-24 22:20:17 -0400

    make dwm not send error checking version

--- dwm-6.2/dwm.c
+++ dwm-6.2/dwm.c
@@ -2101,10 +2101,13 @@ zoom(const Arg *arg)
 }
 
 int
+
 main(int argc, char *argv[])
 {
-	if (argc == 2 && !strcmp("-v", argv[1]))
-		die("dwm-"VERSION);
+	if (argc == 2 && !strcmp("-v", argv[1])){
+		printf("dwm-"VERSION);
+		return 0;
+	}
 	else if (argc != 1)
 		die("usage: dwm [-v]");
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
